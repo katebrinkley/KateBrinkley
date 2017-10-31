ec5ee4fe024f439e8b9907fb75ec2bca74ed69c4
Kate Brinkley <brinkley.kaitlin.m@gmail.com>
2017-10-31 18:32:50 -0400

    Class 12 Homework

---
+++ 12/Assignment/starter_code/js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+var cityOptions = [
+	"NYC",
+	"SF",
+	"LA",
+	"ATX",
+	"SYD"
+];
+
+var cityType = "cityTypeValue"
+
+/*Create a loop to display options*/
+
+for (var i = 0; i < cityOptions.length; i++) {
+    $("#city-type").append("<option>" + "NYC" + "</option>");
+} 
+
+/*get user input value*/
+
+/* If city-type equals NewYork, NYC or New York City, then change the background*/
+
+if(cityTypeValue === "NYC") {
+	$('body').addClass('.nyc');
+}
+
+/*Stop form from submitting*/
+
+});
