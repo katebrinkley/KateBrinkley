e0c424c9422a9afc3a43ae8554360338d265a6ce
Kate Brinkley <brinkley.kaitlin.m@gmail.com>
2017-10-31 18:32:42 -0400

    Review work

--- 10/homework/starter_code/js/index.js
+++ 10/homework/starter_code/js/index.js
@@ -3,99 +3,22 @@
 
 $(document).ready(function(){
 
-	$()
+$("form").submit(function(event){
+	var inputField = $("input").val();
 
-
-var ny = [
-	"New York",
-	"NYC",
-	"New York City"
-]
-
-var sf = [
-	"San Francisco",
-	"SF",
-	"Bay Area"
-]
-
-var la =[
-	"Los Angeles",
-	"LA",
-	"LAX"
-]
-
-var austin =[
-	"Austin",
-	"ATX"
-]
-
-var sydney =[
-	"Sydney",
-	"SYD"
-]
-
-
-/* Pseudocode
-
-Input city name*/
-
-/*While city input is none
-
-	keep background to citipix_skyline.jpg*/
-
-/*if input is New York, New York City or NYC
-
-AND button is clicked
-
-	change background to nyc.jpg
-
-Else 
-
-	keep background to citipix_skyline
-
-*/
-
-$("form").submit (function(){
-	$("input:text").val("ny");
-	$("body").css("color", "blue")
-});
-
-if("input: text" = "ny") {
-			i = background.attr ;
-		}
-
-
-
-/*$("form").submit (function(event){
-	$("input:text").val("ny");
-	event.preventDefault();
-});*/
-
-/*f input is San Francisco, SF or Bay Area
-
-	change background to sf.jpg*/
-
-/*if input is Los Angeles, LA or LAX
-
-	change background to la.jpg*/
-
-/*If input is Sydney or SYD
-
-	change background to sydney.jpg*/
-
-/*If input is Austin or ATX
-
-	change background to austin.jpg*/
+	if(inputField === "New York" || inputField === "NYC" || 
+		inputField === "New York City") {
+	/*$('body').css("background", 'url('./images/nyc.jpg'))'*/
+$('body').addClass('nyc';)
+	} else if (inputField ==='SF') {
+		$('body').addClass('sf')
+	}
+})
 
 
 
 
 
-
-
-
-
-
-
+ 	event.preventDefault();
 
 });
