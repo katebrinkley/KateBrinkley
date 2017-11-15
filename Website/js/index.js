50e6054484cac5b510a79ee6532ec95006ae9096
Kate Brinkley <brinkley.kaitlin.m@gmail.com>
2017-11-14 21:41:22 -0500

    Class website lab

--- Website/js/index.js
+++ Website/js/index.js
@@ -1,13 +1,43 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
 
-$("button").click(function(){
-        $("button").attr("href", "file:///Users/kevinbrinkley/Sites/KateBrinkley/Website/signup.html");
-    });
+// $("button").click(function(){
+//         $("button").attr("href", "file:///Users/kevinbrinkley/Sites/KateBrinkley/Website/signup.html");
+//     });
 
-$('.hamburgerMenu').click(function(){
-	$('nav').slideToggle('slow');
-});
+// $('.hamburgerMenu').click(function(){
+// 	$('nav').slideToggle('slow');
+// };
 
-}
+// }
+
+// 	var images = [
+// 		'images/outfit1.jpg',
+// 		'images/outfit2.jpg',
+// 		'images/outfit3.jpg',
+// 	];
+
+// 	i = 0;
+
+// 	$('#prevButton').click(function(){
+// 		i--;
+
+// 		if(i < 0) {
+// 			i = images.length - 1;
+// 		}
+// console.log(i);
+// 		$('#image').attr('src',images[i]);
+// 	});
+
+// 	$("#nextButton").click(function(){
+// 		i++;
+
+// 		if(i >= images.length) {
+// 			i = 0;
+// 		}
+// 		console.log(i);
+// 		$("#image").attr("src", images[i]);
+// 	});
+
+// });
