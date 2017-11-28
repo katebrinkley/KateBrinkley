fb07e14f7ab0a7950394cdfab10d1f8432cc1b63
Kate Brinkley <brinkley.kaitlin.m@gmail.com>
2017-11-28 00:02:02 -0500

    Website current changes

---
+++ Website/js/catalog.js
@@ -0,0 +1,13 @@
+document.getElementById('bamboo').onclick = showBamboo;
+document.getElementById('cotton').onclick = showCotton;
+
+function showBamboo() {
+  document.body.style.backgroundColor = 'gray';
+  document.body.style.color = 'white';
+  console.log 
+}
+
+function showCotton() {
+  document.body.style.backgroundColor = 'red';
+  document.body.style.color = 'black';
+}
--- Website/js/index.js
+++ Website/js/index.js
@@ -1,72 +1,71 @@
 $(document).ready(function(){
 
-	//Window scroll
+//SMOOTH SCROLL FOR NAV BAR (DESKTOP)
+
 	$("nav.toggleNav a").click(function(e) {
 		var id = $(this).attr('href');
 
 	    $('html, body').animate({
 	        scrollTop: $(id).offset().top
-	    }, 2000);
+	    }, 200);
 
 	    e.preventDefault();
 	});
 });
 
 
+//MENU TOGGLE (ON MOBILE)
+
 $('.hamburgerMenu').click(function(){
     $('nav').slideToggle(200);
 });
 
-//Slick carousel
-$('.gridRow').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+//SLIDER BOXES FOR FABRIC TYPES
+
+
+$('.bambooToggle').click(function(){
+	$('.bambooSlide').slideToggle('slow');
+});
+
+
+$('.silkToggle').click(function(){
+	$('.silkSlide').slideToggle('slow');
+});
+
+$('.cottonToggle').click(function(){
+	$('.cottonSlide').slideToggle('slow');
+});
+
+$('.woolToggle').click(function(){
+	$('.woolSlide').slideToggle('slow');
 });
 
 
 
+// SLICK SLIDER 
 
-// $(document).ready(function(){
+$('.boxes').slick({
+  infinite: false,
+  slidesToShow: 5,
+  slidesToScroll: 10,
+  autoplay: false,
+  autoplaySpeed: 4000,
 
+});
 
+prevArrow: $('.prev')
+nextArrow: $('.next')
 
-// $("button").click(function(){
-//   $("button").attr("href", "file:///Users/kevinbrinkley/Sites/KateBrinkley/Website/signup.html");
-// });
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
 
-// $('.hamburgerMenu').click(function(){
-// 	$('nav').slideToggle('slow');
-// };
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
 
-// }
+	
+
 
 // 	var images = [
 // 		'images/outfit1.jpg',
